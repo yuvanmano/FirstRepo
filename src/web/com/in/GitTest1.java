d213648aac3bab9b672f8a95c2b152dab9cf3510
yuvanmano <https://github.com/yuvanmano/FirstGIT.git>
2021-11-09 06:37:36 +0530

    firstcommit

--- src/web/com/in/GitTest1.java
+++ src/web/com/in/GitTest1.java
@@ -4,6 +4,7 @@ public class GitTest1 {
 
 	public static void main(String[] args) {
 		System.out.println("welcome");
+		System.out.println("Hi");
 
 	}
 
--- src/web/com/in/GitTest2.java
+++ src/web/com/in/GitTest2.java
@@ -2,7 +2,7 @@ package web.com.in;
 
 public class GitTest2 {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("good morning");
 	}
 
 }
